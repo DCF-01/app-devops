e91818f3e7b8250e0bf6f16860b71edad828ecbb
RadoslavKezunovski <RadoslavKezunovski@DESKTOP-OM8V17G>
2023-01-25 13:04:35 +0100

    init

---
+++ app2/Program.cs
@@ -0,0 +1,10 @@
+﻿while (true)
+{
+    using var client = new HttpClient();
+    var pingUri = "https://google.com";
+        
+    client.BaseAddress = new Uri(pingUri);
+    var res = client.GetAsync("/");
+    Console.WriteLine($"App2 pinged {pingUri}, response: {(int)res.Result.StatusCode} {res.Result.StatusCode}");
+    Thread.Sleep(1000);
+}
